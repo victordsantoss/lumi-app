8e82841f7e002653a849e621ebd7f33e912f3320
Victor - Trabalho <victorsamueldossantoslucas@MacBook-Pro-de-Victor.local>
2025-04-04 16:24:08 -0300

    styles: update styles

--- src/templates/dashboard/components/summary-card.tsx
+++ src/templates/dashboard/components/summary-card.tsx
@@ -6,7 +6,12 @@ import Paper from '@mui/material/Paper';
 import { IDashboardViewProps } from '../dashboard.types';
 import { useTheme } from '@mui/material/styles';
 
-export default function SummaryCard({ totalCompensatedEnergy, totalElectricalEnergy, totalInvoiceAmount, averageInvoiceAmount }: IDashboardViewProps) {
+
+export interface ISummaryCardProps extends IDashboardViewProps {
+  totalInvoices: number;
+}
+
+export default function SummaryCard({ totalCompensatedEnergy, totalElectricalEnergy, totalInvoiceAmount, averageInvoiceAmount, totalInvoices }: ISummaryCardProps) {
   return (
     <Card sx={{ backgroundColor: 'black' }}>
       <CardContent
@@ -16,10 +21,11 @@ export default function SummaryCard({ totalCompensatedEnergy, totalElectricalEne
           gap: 2,
         }}
       >
+        <SummaryPaper title={'Quantidade de Faturas'} value={totalInvoices} />
         <SummaryPaper title={'Valor Total das Faturas'} value={totalInvoiceAmount} isCurrency />
         <SummaryPaper title={'Energia Elétrica Total'} value={totalElectricalEnergy} isEnergy />
         <SummaryPaper title={'Valor Médio das Faturas'} value={averageInvoiceAmount} isCurrency />
-        <SummaryPaper title={'Energia Compensada Total'} value={totalCompensatedEnergy} isEnergy />
+        <SummaryPaper title={'Energia Comp. Total'} value={totalCompensatedEnergy} isEnergy />
       </CardContent>
     </Card>
   );
@@ -44,11 +50,17 @@ export const SummaryPaper = ({ title, value, isCurrency, isEnergy }: ISummaryPap
     if (isEnergy) {
       return `${value.toLocaleString('pt-BR')} kWh`;
     }
-    return value.toLocaleString('pt-BR');
+    return value;
   };
 
   return (
-    <Paper sx={{ width: '100%', p: 2, backgroundColor: 'primary.main', border: `1px solid ${theme.palette.background.default}` }}>
+    <Paper sx={{
+      width: '100%',
+      flex: 1,
+      p: 2,
+      backgroundColor: 'primary.main',
+      border: `1px solid ${theme.palette.background.default}`
+    }}>
       <Typography variant="h6">{title}</Typography>
       <Typography variant="h3" fontWeight={700}>{formatValue()}</Typography>
     </Paper>
--- src/templates/dashboard/dashboard.view.tsx
+++ src/templates/dashboard/dashboard.view.tsx
@@ -24,7 +24,7 @@ export const DashboardView = ({
         averageInvoiceAmount={averageInvoiceAmount}
         totalCompensatedEnergy={totalCompensatedEnergy}
         totalElectricalEnergy={totalElectricalEnergy}
-        invoices={invoices}
+        totalInvoices={invoices.data.length}
       />
       <Typography variant="h4">Dashboard</Typography>
     </Box>
